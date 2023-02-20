8598951c2e4c62c97f5f84b4225a3d25ab214782
yusun97 <yusun.apply@outlook.com>
2023-02-20 00:32:22 -0800

    add screens templates

---
+++ screens/allEntriesScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function allEntriesScreen() {
+  return (
+    <View>
+      <Text>allEntriesScreen</Text>
+    </View>
+  )
+}
---
+++ screens/overLimitEntriesScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function overLimitEntriesScreen() {
+  return (
+    <View>
+      <Text>overLimitEntriesScreen</Text>
+    </View>
+  );
+}
