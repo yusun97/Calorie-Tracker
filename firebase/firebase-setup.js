0f854195775fa9b1a91f2709aa5e7a5794122aaf
yusun97 <yusun.apply@outlook.com>
2023-02-26 20:59:03 -0800

    using environment variable for firebase config information

--- firebase/firebase-setup.js
+++ firebase/firebase-setup.js
@@ -1,18 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+} from "@env";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCSPL5C3MjTflzql_iU29Rc7wcE2VTSaFo",
-  authDomain: "assignmenttwo-900ac.firebaseapp.com",
-  projectId: "assignmenttwo-900ac",
-  storageBucket: "assignmenttwo-900ac.appspot.com",
-  messagingSenderId: "173786567677",
-  appId: "1:173786567677:web:d3d423f3799070a43cff94",
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
 };
 
 // Initialize Firebase
