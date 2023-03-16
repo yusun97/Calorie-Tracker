8453ad8d39e87501ea81ceb497368d0d354a7f93
yusun97 <yusun.apply@outlook.com>
2023-03-15 21:44:56 -0700

    add buttons on login page

--- screens/Login.js
+++ screens/Login.js
@@ -1,52 +1,84 @@
-import { View, Text, Modal } from "react-native";
-import React from "react";
+// import { View, Text, Modal } from "react-native";
+// import React from "react";
 import LinearGradientBackground from "../components/LinearGradientBackground";
 import Card from "../components/Card";
 import PressableButton from "../components/PressableButton";
-import { StyleSheet } from "react-native";
 import { lightPurple, darkPurple, white } from "../components/CommonStyle";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { auth } from "../firebase/firebase-setup";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function loginHandle() {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function signUpHandle() {
+    navigation.replace("SignUp");
+  }
+
   return (
     <LinearGradientBackground>
-      <View style={styles.genericContainer}>
-        <Text>Welcome to the Login in Page</Text>
+      <View style={styles.container}>
+        <Text>Email Address</Text>
+        <TextInput
+          style={styles.textInput}
+          value={email}
+          onChangeText={(email) => {
+            setEmail(email);
+          }}
+          placeholder="Email"
+        />
+        <Text>Password</Text>
+        <TextInput
+          style={styles.textInput}
+          secureTextEntry={true}
+          value={password}
+          onChangeText={(password) => {
+            setPassword(password);
+          }}
+          placeholder="Password"
+        />
+        <PressableButton buttonPressed={loginHandle}>
+          <Text style={styles.buttonText}>Login In</Text>
+        </PressableButton>
 
-        <View style={styles.buttonContainer}>
-          <PressableButton>
-            <Text style={styles.buttonText}>Login in</Text>
-          </PressableButton>
-          <PressableButton>
-            <Text style={styles.buttonText}>Sign Up</Text>
-          </PressableButton>
-        </View>
+        <PressableButton buttonPressed={signUpHandle}>
+          <Text style={styles.buttonText}>
+            New Users ? Create a new account
+          </Text>
+        </PressableButton>
       </View>
     </LinearGradientBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  genericContainer: {
+  container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
-  container: {
-    padding: 10,
-    paddingTop: 70,
-    margin: 10,
-  },
-  text: {
-    color: darkPurple,
-    fontWeight: "bold",
-    fontSize: 20,
-  },
-  buttonContainer: {
-    // margin: 20,
-    // padding: 20,
-    justifyContent: "space-between",
-    alignItems: "space-between",
-  },
+  textInput: { borderWidth: 3, width: 200, height: 30 },
+  // text: {
+  //   color: darkPurple,
+  //   fontWeight: "bold",
+  //   fontSize: 20,
+  // },
+  // buttonContainer: {
+  //   // margin: 20,
+  //   // padding: 20,
+  //   justifyContent: "space-between",
+  //   alignItems: "space-between",
+  // },
   buttonText: {
     color: white,
     fontSize: 15,
@@ -54,16 +86,16 @@ const styles = StyleSheet.create({
     paddingLeft: 30,
     paddingRight: 30,
   },
-  smallTextContainer: {
-    backgroundColor: lightPurple,
-    borderRadius: 5,
-    height: 30,
-    width: 200,
-  },
-  bigTextContainer: {
-    backgroundColor: lightPurple,
-    borderRadius: 5,
-    height: 160,
-    width: 200,
-  },
+  // smallTextContainer: {
+  //   backgroundColor: lightPurple,
+  //   borderRadius: 5,
+  //   height: 30,
+  //   width: 200,
+  // },
+  // bigTextContainer: {
+  //   backgroundColor: lightPurple,
+  //   borderRadius: 5,
+  //   height: 160,
+  //   width: 200,
+  // },
 });
